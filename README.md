fad3d89f1fe20fc5da1b73d6d00ed5d65fd7a56a
VitaliyBeletskiy <vitaliy.beletskiy@gmail.com>
2021-03-23 13:15:52 +0100

    added ABout/Version to Settings

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Darts Training Calculator (Work In Progress)
 
 The app counts scores during Darts training session.
+
+To-do:
+- добавить возможность кастомной игры (301, 501, введите своё значение (>1))
--- app/src/main/java/com/beletskiy/dartstrainingcalculator/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/beletskiy/dartstrainingcalculator/fragments/settings/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.beletskiy.dartstrainingcalculator.fragments.settings
 
+import android.content.pm.PackageInfo
 import android.os.Bundle
 import android.util.Log
 import androidx.preference.Preference
@@ -7,10 +8,20 @@ import androidx.preference.PreferenceFragmentCompat
 import com.beletskiy.dartstrainingcalculator.R
 import com.beletskiy.dartstrainingcalculator.utils.TAG
 
+
 class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        val appVersion = requireContext().packageManager.getPackageInfo(
+            requireContext().packageName,
+            0
+        ).versionName
+        val summaryProvider = Preference.SummaryProvider<Preference> {
+            "Version $appVersion"
+        }
+        findPreference<Preference>("version")?.summaryProvider = summaryProvider
 
         // set Preference Change Listeners
         val preventSleepPreference: Preference? =
@@ -21,7 +32,10 @@ class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChan
     override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
         preference?.let {
             if (preference.key == getString(R.string.prevent_sleep_key)) {
-                Log.i(TAG, "onPreferenceChange: preventSleepPreference CHANGED, new value = $newValue")
+                Log.i(
+                    TAG,
+                    "onPreferenceChange: preventSleepPreference CHANGED, new value = $newValue"
+                )
                 // TODO: 22/03/2021 handle "prevent phone from sleeping"
             }
         }
